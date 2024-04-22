5fe17589031a959f0af8522f78cce10cb3863781
Oleksii Siianchuk <94801705+Jleha@users.noreply.github.com>
2024-04-22 23:35:24 +0300

    init commit

---
+++ src/main/java/ua/lviv/marazyncheck/MarazyncheckApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.marazyncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarazyncheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarazyncheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lviv/marazyncheck/MarazyncheckApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.marazyncheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarazyncheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
